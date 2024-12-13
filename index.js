a0d36c0942a5b9081835c36f03c6f776b3cdf95b
Atomic <80856947+AtomicTyler1@users.noreply.github.com>
2024-12-13 20:17:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["TEST", "TEST"];
+const statusMessages = ["💎 Our staff team can help", "🎫 Use the ticket system!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
